0ff231a9d114578ab0b6bd3207d5f8959bdcdf4a
Nemwel-Boniface <nemwelboniface@outlook.com>
2025-02-13 17:35:20 +0300

    Update included:   - Generated the borrowings controller with the needed actions in it   - Updated the actions available with the functionality desired   - Included the borrowing views to be updated

---
+++ app/controllers/borrowings_controller.rb
@@ -0,0 +1,26 @@
+class BorrowingsController < ApplicationController
+  def index
+    @borrowing = current_user.borrowings.includes(:book)
+  end
+
+  def new
+    @borrowing = Borrowing.new
+  end
+
+  def create
+    @borrowing = current_user.borrowings.build(borrowing_params)
+
+    if @borrowing.save
+      @borrowing.book.update(available: false)
+      redirect_to borrowings_path, notice: 'Book was successfully borrowed.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def borrowing_params
+    params.require(:borrowing).permit(:book_id)
+  end
+end
