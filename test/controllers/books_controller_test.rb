0248b88fbac407a04ac6b5e161707116fd4b8f1d
Nemwel-Boniface <nemwelboniface@outlook.com>
2025-02-16 00:40:31 +0300

    Update included book controller spec

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,28 +1,85 @@
 require "test_helper"
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  include ActionDispatch::TestProcess::FixtureFile
+  setup do
+    @user = users(:one)
+    sign_in users(:one)
+    @book = books(:one)
+    sign_in @user
+  end
+
   test "should get index" do
-    get books_index_url
+    get books_url
     assert_response :success
   end
 
   test "should get show" do
-    get books_show_url
+    get book_url(@book)
     assert_response :success
   end
 
-  test "should get new" do
-    get books_new_url
+  test "should redirect new if user is not admin" do
+    sign_in @user
+    get new_book_url
+    assert_redirected_to root_path
+    assert_equal 'Access denied.', flash[:alert]
+  end
+
+  test "should create book" do
+    sign_in @user
+    assert_difference('Book.count', 1) do
+      post books_url, params: {
+        book: {
+          title: 'New Book',
+          author: 'Author',
+          isbn: '12345',
+          description: 'Description',
+          cover_photo: fixture_file_upload(Rails.root.join('test/fixtures/files/The-Mountain-Is-You-nuriakenya.jpg'), 'image/jpeg')
+        }
+      }
+    end
+  end
+
+  test "should not create book with invalid data" do
+    sign_in @user
+    assert_no_difference('Book.count') do
+      post books_url, params: { book: { title: '', author: 'Author', isbn: '', description: 'Description' } }
+    end
+  end
+
+  test "should get edit if user is admin" do
+    sign_in @user
+    get edit_book_url(@book)
     assert_response :success
   end
 
-  test "should get edit" do
-    get books_edit_url
-    assert_response :success
+  test "should redirect edit if user is not admin" do
+    sign_in @user
+    get edit_book_url(@book)
+    assert_redirected_to root_path
   end
 
-  test "should get destroy" do
-    get books_destroy_url
-    assert_response :success
+  test "should update book" do
+    sign_in @user
+    patch book_url(@book), params: { book: { title: 'Updated Book', author: 'Updated Author', isbn: '67890', description: 'Updated description' } }
+    assert_redirected_to book_url(@book)
+    @book.reload
+    assert_equal 'Updated Book', @book.title
+  end
+
+  test "should not update book with invalid data" do
+    sign_in @user
+    patch book_url(@book), params: { book: { title: '', author: '', isbn: '', description: '' } }
+    assert_template :edit
+  end
+
+  test "should destroy book" do
+    sign_in @user
+    assert_difference('Book.count', -1) do
+      delete book_url(@book)
+    end
+    assert_redirected_to books_url
   end
 end
