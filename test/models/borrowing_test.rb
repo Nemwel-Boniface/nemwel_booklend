33529f5091a199eb04ebbc515fb74ab5e7e0aa67
Nemwel-Boniface <nemwelboniface@outlook.com>
2025-02-15 17:35:52 +0300

    Update included:   - Created sample borrowing details in the borrowing fixer   - Created sample model specs

--- test/models/borrowing_test.rb
+++ test/models/borrowing_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class BorrowingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @borrowing = borrowings(:one)
+  end
+
+  test "should be valid with valid attributes" do
+    assert @borrowing.valid?
+  end
+
+  test "should require a user" do
+    @borrowing.user = nil
+    assert_not @borrowing.valid?
+    assert_includes @borrowing.errors[:user], "must exist"
+  end
+
+  test "should require a book" do
+    @borrowing.book = nil
+    assert_not @borrowing.valid?
+    assert_includes @borrowing.errors[:book], "must exist"
+  end
+
+  test "should belong to a user" do
+    assert_respond_to @borrowing, :user
+  end
+
+  test "should belong to a book" do
+    assert_respond_to @borrowing, :book
+  end
 end
