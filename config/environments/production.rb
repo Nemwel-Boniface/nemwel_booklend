4aebd19e163076e0548d300daf8e9677660dcbc5
Nemwel-Boniface <nemwelboniface@outlook.com>
2025-02-16 14:28:50 +0300

    Add secret key base to production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,9 @@ Rails.application.configure do
   # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
   config.eager_load = true
 
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
+
   # Full error reports are disabled.
   config.consider_all_requests_local = false
 
